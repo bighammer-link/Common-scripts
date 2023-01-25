68df6d1e89e58ed7fc7fa31256ffb502362c169a
bighammer <3225579752@qq.com>
2023-01-25 12:25:53 +0800

    微博超话签到

---
+++ weibo.py
@@ -0,0 +1,53 @@
+import json
+
+import requests
+import re
+import time
+
+# 这里是你要签到的超话的id，每个超话的不一样，多个的话请用英文逗号隔开，例如‘[AA’,‘BB’]，进入到超话界面，地址栏中的地址的p/和/super_index之间的这一串
+ids = ['']
+# 这里是选择Server酱的推送方式，填写Sendkey，不想使用的话直接忽略
+SCKEY = ''
+# 这里填写你的COOKIE
+cookie = ''
+url = 'https://weibo.com/p/aj/general/button?ajwvr=6'
+
+
+content = ''
+num = 0
+header = {
+    'cookie': cookie,
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+}
+for person_id in ids:
+    info_url = 'https://weibo.com/p/{}/super_index'.format(person_id)
+    data = {
+    'api': 'http://i.huati.weibo.com/aj/super/checkin',
+    'texta': '%E5%B7%B2%E7%AD%BE%E5%88%B0',
+    'textb': '%E5%B7%B2%E7%AD%BE%E5%88%B0',
+    'status': '1',
+    'id': person_id,
+    'location': 'page_100808_super_index',
+    'timezone': 'GMT+0800',
+    'lang': 'zh-cn',
+    'plat': 'Win32',
+    'ua': 'Mozilla/5.0%20(Windows%20NT%2010.0;%20Win64;%20x64)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Chrome/109.0.0.0%20Safari/537.36',
+    'screen': '1536*864',
+    '__rnd': '1674614466131'}
+    try:
+        # 获取超话社区名字
+        html_info =requests.get(url=info_url, headers=header).text
+        info = "".join(re.findall('<title>(.*?)</title>', html_info, re.S))
+        info = info.replace('—新浪微博超话社区', '')
+        #进行签到
+        response = requests.get(url=url, headers=header, params=data).text
+        result = json.loads(response)
+        content =content+info+'   '+result['msg']+'\n\n'
+        num += 1
+        if num ==len(ids):
+          if SCKEY !='':
+            requests.post("https://sctapi.ftqq.com/{}.send?title={}&desp={}".format(SCKEY, "微博超话签到", content))    
+    except:
+        content = '签到失败，可能是cookie失效,请及时更新cookie'
+        if SCKEY !='':
+            requests.post("https://sctapi.ftqq.com/{}.send?title={}&desp={}".format(SCKEY, "微博超话签到", content))
