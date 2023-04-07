876e2905f9d62a3974470a58e60127996810f09b
bighammer <3225579752@qq.com>
2023-04-07 15:39:02 +0800

    微博超话

--- weibo.py
+++ weibo.py
@@ -4,12 +4,28 @@ import requests
 import re
 import time
 
-# 这里是你要签到的超话的id，每个超话的不一样，多个的话请用英文逗号隔开，例如‘[AA’,‘BB’]，进入到超话界面，地址栏中的地址的p/和/super_index之间的这一串
+# 这里是你要签到的超话的id，每个超话的不一样，多个的话请用英文逗号隔开，例如‘[AA’,‘BB’].获取方式：进入到超话界面，地址栏中的地址的p/和/super_index之间的这一串
 ids = ['']
 # 这里是选择Server酱的推送方式，填写Sendkey，不想使用的话直接忽略
 SCKEY = ''
+# 推送PLUS的token
+Token = ''
 # 这里填写你的COOKIE
 cookie = ''
+# 推送函数
+def push(content):
+    if SCKEY != '':
+        url = "https://sctapi.ftqq.com/{}.send?title={}&desp={}".format(SCKEY, '微博超话签到', content)
+        requests.post(url)
+        print('推送完成')
+    elif Token != '':
+        headers = {'Content-Type': 'application/json'}
+        json = {"token": Token, 'title': '微博超话签到', 'content': content, "template": "json"}
+        resp = requests.post(f'http://www.pushplus.plus/send', json=json, headers=headers).json()
+        print('push+推送成功' if resp['code'] == 200 else 'push+推送失败')
+    else:
+        print('未使用消息推送推送！')
+
 url = 'https://weibo.com/p/aj/general/button?ajwvr=6'
 
 
@@ -45,9 +61,7 @@ for person_id in ids:
         content =content+info+'   '+result['msg']+'\n\n'
         num += 1
         if num ==len(ids):
-          if SCKEY !='':
-            requests.post("https://sctapi.ftqq.com/{}.send?title={}&desp={}".format(SCKEY, "微博超话签到", content))    
+          push(content)
     except:
         content = '签到失败，可能是cookie失效,请及时更新cookie'
-        if SCKEY !='':
-            requests.post("https://sctapi.ftqq.com/{}.send?title={}&desp={}".format(SCKEY, "微博超话签到", content))
+        push(content)
